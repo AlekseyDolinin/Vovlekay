3159641d9beaab2a8590d3698b57ac0e485e0697
allDolinin <allDolinin@gmail.com>
2024-07-06 14:21:38 +0300

    Initial commit

---
+++ VovlekayApp.swift
@@ -0,0 +1,17 @@
+//
+//  VovlekayApp.swift
+//  Vovlekay
+//
+//  Created by Aleksey Dolinin on 06.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct VovlekayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
