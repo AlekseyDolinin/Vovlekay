0d5d9d174433bf5dc275133129771e96b66805f4
allDolinin <allDolinin@gmail.com>
2024-07-12 16:31:20 +0300

    v

--- LocalStorage.swift
+++ LocalStorage.swift
@@ -28,22 +28,7 @@ extension LocalStorage {
                     if cookie.name == "user_id" && cookie.domain == Endpoint.hostname {
                         var cookieDict = [String : AnyObject]()
                         cookieDict[cookie.name] = cookie.properties as AnyObject?
-                        // save cookies
-//                        UserDefaults.standard.set(cookieDict, forKey: .cookiesKey)
-//                        print(cookieDict[cookie.name])
-//                        appDelegate.loadDataForStart()
-                        
-//                        if let cookie = cookieDict[cookie.name] {
-//                            LocalStorage.saveCookies(cookie: cookie)
-//                        }
-                        print(cookieDict)
-                        
                         guard let dictionary = cookieDict["user_id"] as? Dictionary<String, Any> else { return }
-                        
-                        print(dictionary["Name"])
-                        print(dictionary["Value"])
-                        
-                        
                         let name: String = dictionary["Name"] as! String
                         let value: String = dictionary["Value"] as! String
                         
@@ -54,31 +39,20 @@ extension LocalStorage {
                         catch let error {
                             print(error)
                         }
-                        
-//                        guard let dictionary = savedCookie["user_id"] as? Dictionary<String, Any> else { return }
-//                        guard let value: String = dictionary["Value"] as? String else { return }
-//                        guard let name: String = dictionary["Name"] as? String else { return }
-                        
-//                        LocalStorage.saveCookies(cookie: cookieDict[cookie.name], key: "cookie_user_id")
                     }
                 }
             }
         }
-
+        
+        // removeCookiesFromKeychain
+        class func removeCookiesFromKeychain() {
+            do {
+                try LocalStorage.keychain.remove("name")
+                try LocalStorage.keychain.remove("value")
+                print("Cookies in Keychain is clear")
+            } catch let error {
+                print("error remove keychain: \(error)")
+            }
+        }
     }
-    
-//    class func saveCookies(cookie: Any?, key: String) {
-//        
-//        if cookie == nil { return }
-//        
-//        let cookieString: String = "\(cookie!)"
-//        
-//        do {
-//            try keychain.set(cookieString, key: key)
-//        }
-//        catch let error {
-//            print(error)
-//        }
-//        
-//    }
 }
--- _ServicesLayer/Auth.swift
+++ _ServicesLayer/Auth.swift
@@ -7,25 +7,17 @@ class Auth {
     /// true - авторизация пройдена
     /// false - авторизация не пройдена
     class func checkAuth() -> Bool {
-        print("checkAuth")
         
         let name = LocalStorage.keychain["name"]
         let value = LocalStorage.keychain["value"]
         
-        print(name)
-        print(value)
-        
+        // если сохранены name и value из cookies то пользователь авторизован
+        // актуальность value проверится при первом запросе
         if name == nil && value == nil {
             return false
         } else {
             // keychain remove
-            do {
-                try LocalStorage.keychain.remove("name")
-                try LocalStorage.keychain.remove("value")
-                print("keychain is clear")       
-            } catch let error {
-                print("error remove keychain: \(error)")
-            }
+            LocalStorage.Cookies.removeCookiesFromKeychain()
             return true
         }
     }
